be21c63e89cb36fdcb255d0e24d5e1ad05d9064c
George <george@skamnos.com>
2017-10-05 18:46:59 +0300

    Viator-ATB DB match Complete.Api almost complete except book-amend functions.
    Signed-off-by: George <george@skamnos.com>

--- src/main/java/Controller/Application.java
+++ src/main/java/Controller/Application.java
@@ -1,9 +1,9 @@
 package Controller;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 /**
  * Created by George on 29/05/17.
@@ -11,6 +11,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 //todo change mysql driver and configure mysql for connection pooling
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
